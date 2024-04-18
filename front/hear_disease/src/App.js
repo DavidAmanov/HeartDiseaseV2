702d38bb11c5ef8d9a4cf4389517e7176bfdfa2d
David Ladaev <174save@gmail.com>
2024-04-18 23:11:02 +0300

    create a simp;e form

--- front/hear_disease/src/App.js
+++ front/hear_disease/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const metricsArray = ['Age', 'Type of chest pain', 'Level of blood pressure (mm/HG)', 'Serum cholesterol in mg/dl',
+  "Blood sugar levels on fasting ", 'Result of electrocardiogram', 'Maximum heart rate achieved', 'Angina induced by exercise', 'Exercise induced ST-depression',
+  'ST segment measured in terms of slope during peak exercise', 'The number of major vessels ', 'A blood disorder called thalassemia']
+
+  const {register, formState:{errors}, handleSubmit} = useForm();
+  const onSubmit = (data)=>{
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <section>
+    <form onSubmit={handleSubmit(onSubmit)} className='form'>
+    {metricsArray.map((item, index) => (
+      <label key={index}>
+        <input {...register(item, { required: true })} className="formItem" id={index} type="text" placeholder={item} />
+      </label>
+    ))}
+    <div className='buttonDiv'>
+      <button type='submit'>submit</button>
     </div>
+    </form>
+  </section>
+  </>
   );
 }
 
