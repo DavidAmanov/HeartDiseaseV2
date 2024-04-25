faf11fb41e18812de7574465f2e2bc320a65c941
David Ladaev <174save@gmail.com>
2024-04-25 22:35:03 +0300

    add some

---
+++ back/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = 3001;
+
+const app = express();
+
+app.listen(PORT, ()=>{
+    console.log('Server is starting');
+});
+
+
+app.post('/api',(req, res)=>{
+    
+})
